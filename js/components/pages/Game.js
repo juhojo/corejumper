9908fbfe302725a596d3b345b77061729e23ecd5
Ilpo Oksanen <ilpo.oksanen@outlook.com>
2016-12-18 22:57:39 +0200

    Changed backwards navigation from esc to left-arrow

--- js/components/pages/Game.js
+++ js/components/pages/Game.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class Game extends Component{
   componentDidMount(){
     this.props.setKeybinds({
-      27: this.props.exit,
+      37: this.props.exit,
       8: this.props.exit,
     });
   }
