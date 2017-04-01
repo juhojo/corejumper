de5513e4bf085cb77b8accb93b6e6dc9de35cdd5
Juho Jokela <juho.jokela@atalent.fi>
2017-04-01 15:54:23 +0300

    Fixed issue #13

--- js/components/reusable/Centerer.js
+++ js/components/reusable/Centerer.js
@@ -5,7 +5,7 @@ export default class Centerer extends Component{
     return (
       <div className='centerer-outer' style={this.props.style}>
         <div className='centerer-middle'>
-          <div className='centerer-inner'>
+          <div className='centerer-inner' style={this.props.innerStyle}>
             {this.props.children}
           </div>
         </div>
--- js/components/reusable/SubPageContent.js
+++ js/components/reusable/SubPageContent.js
@@ -5,17 +5,18 @@ import Centerer from '../reusable/Centerer';
 
 export default class SubPageContent extends Component{
   render (){
+    console.log(this.props.style);
     return (
-      <div id={this.props.id} className="page sub-page" style={this.props.style}>
+      <div id={ this.props.id } className="page sub-page" style={ this.props.style }>
         <Scrollbar>
           <div className="content">
-            <Centerer>
+            <Centerer innerStyle={{ paddingBottom: '8vh' }}>
               {this.props.children}
             </Centerer>
           </div>
         </Scrollbar>
         <div className="header-fade"></div>
-        <MenuButton exit={this.props.exit} />
+        <MenuButton exit={ this.props.exit } />
       </div>
     );
   }
